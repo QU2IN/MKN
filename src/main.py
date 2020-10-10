aa064f2b90ae2b746cb0d12eb2af6c20d074a500
motiops <matcherr@yandex.ru>
2020-10-10 13:19:39 +0300

    add db

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from config import DevConfig
+from apps.admin.apps import admin_page
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+app.register_blueprint(admin_page)
 db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return '<h1>Hello World!</h1>'
+
+
+if __name__ == '__main__':
+    app.run()
