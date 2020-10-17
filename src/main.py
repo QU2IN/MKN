88ef7bb70dc0f1add5ab160e428036debfa9355f
motka-dev <matcherr@yandex.ru>
2020-10-17 10:59:08 +0300

    connect apps

--- src/main.py
+++ src/main.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from config import DevConfig
-from apps.admin.apps import admin_page
 from flask_sqlalchemy import SQLAlchemy
+
+from apps.admin.views import admin_app
+from apps.auth.views import auth_app
+from apps.blog.views import post_app
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
-app.register_blueprint(admin_page)
+app.register_blueprint(admin_app, url_prefix='/admin')
+app.register_blueprint(auth_app, url_prefix='/auth')
+app.register_blueprint(post_app, url_prefix='/blog')
 db = SQLAlchemy(app)
+
 
 @app.route('/')
 def home():
