73e80d4bd952bc14458ef04a7209bcd35be33c6d
motka-dev <matcherr@yandex.ru>
2020-10-17 10:58:12 +0300

    fix app name

--- src/apps/admin/views.py
+++ src/apps/admin/views.py
@@ -1,10 +1,8 @@
-from flask import Blueprint, render_template, abort
-from jinja2 import TemplateNotFound
 from flask import Blueprint
 
-admin_page = Blueprint('admin_page', __name__, template_folder='templates')
+admin_app = Blueprint('admin', __name__, template_folder='templates')
 
 
-@admin_page.route('/')
+@admin_app.route('/')
 def home():
-    return '<h1>Hello Kek</h1>'
+    return '<h1>Hello Admin</h1>'
